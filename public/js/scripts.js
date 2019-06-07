222978410094b8c3262ecc8d52be39f5b2d84317
Gerardo Keys <jotojry@icloud.com>
2019-06-06 17:57:29 -0700

    added videos to project pages

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -6,7 +6,7 @@ const screenshots = document.querySelectorAll(".image");
 // create gifs on poster screenshots when hovering
 for(let i = 0; i < screenshots.length; i ++) {
     screenshots[i].addEventListener('mouseenter', function(e){
-        console.log(e.target);
+        //console.log(e.target);
     })
 }
     
